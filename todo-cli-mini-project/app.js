b7e6894dd5104e5c42d9318f0b86d4c9bc707c97
AnirudhDev01 <anirudhbantu@gmail.com>
2025-07-23 23:56:11 +0530

    Added the comments for todo-cli-mini-project

--- todo-cli-mini-project/app.js
+++ todo-cli-mini-project/app.js
@@ -1,15 +1,22 @@
+// This project is built by Anirudh
 import readline from 'readline'; // It is a in-built module provided by nodejs
 const rl = readline.createInterface({
     input:process.stdin, // To take the input from console
     output:process.stdout // To show the output on console
 })
+// Creating an empty array to store the todos
 const todos = [];
+// Showing menu for user through console
 const showMenu = () => {
     console.log("\n1. Add a task");
     console.log("2. View all tasks");
     console.log("3. Exit");
     rl.question("Choose an option", handleInput);    
 }
+// Handling the input values which are provided by user 
+// 1 -> for adding new task/todo
+// 2 -> for displaying the todos
+// 3 -> for deleting the task/todo
 const handleInput = (option) =>{
     if(option === "1"){
         rl.question("Enter the task", (task)=>{
@@ -32,5 +39,5 @@ const handleInput = (option) =>{
     }
 }
 
-
+// Showing the menu when the app starts by calling the function
 showMenu();
